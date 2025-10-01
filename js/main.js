275de239818f692aadfd73cf606454933bad99fc
AndriiKusiak <129413305+AndriiKusiak@users.noreply.github.com>
2025-10-01 08:37:31 +0300

    Removed comment in js file

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   document.getElementById("burger").addEventListener("click", function () {
-//     document.querySelector("header").classList.toggle("open");
-//   });
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("burger").addEventListener("click", function () {
     document.querySelector("header").classList.toggle("open");
